861a2a48e486f67a19d4c96a9b311993ddebdc44
bme2003 <bme2003@icloud.com>
2025-12-14 13:00:15 -0700

    Initial Commit

---
+++ SidePot/SidePotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidePotApp.swift
+//  SidePot
+//
+//  Created by Brody England on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SidePotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
